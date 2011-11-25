5fe0d682d31a39ced443cd2d6bf1f1bba4162b6a
CoE#19 <mayaseven@hotmail.com>
2011-11-25 23:03:26 +0700

    asdf

--- src/kku/android/kns/KNSActivity.java
+++ src/kku/android/kns/KNSActivity.java
@@ -9,5 +9,9 @@ public class KNSActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        //MaYaSeVeN
+        //1234
+        //ll
+        //aaa
     }
 }
