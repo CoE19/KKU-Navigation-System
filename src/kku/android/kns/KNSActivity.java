b74cc62c6aca07829081405a690ccee07ed60993
DarthDeveloper <settachok@gmail.com>
2011-11-25 22:49:51 +0700

    Update src/kku/android/kns/KNSActivity.java

--- src/kku/android/kns/KNSActivity.java
+++ src/kku/android/kns/KNSActivity.java
@@ -9,7 +9,7 @@ public class KNSActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        //MaYaSeVeN2
+        //MaYaSeVeN
         //1234
     }
 }
